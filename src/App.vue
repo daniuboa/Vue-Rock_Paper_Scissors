e6d106746bc8f42f10c06961f7ae206f5a468dfe
Dani Uboa <uboadaniel@gmail.com>
2022-07-13 18:29:36 +0300

    Add imports and constants

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,49 @@
 <script setup>
+import { ref, computed, onMounted } from "vue";
+
+const wins = ref(0);
+const draws = ref(0);
+const loses = ref(0);
+
+const choice = ref(null);
+const computerChoice = ref(null);
+const verdict = ref(null);
+
+const outcomes = {
+  rock: {
+    rock: "draw",
+    paper: "loss",
+    scissors: "win",
+  },
+  paper: {
+    rock: "win",
+    paper: "draw",
+    scissors: "loss",
+  },
+  scissors: {
+    rock: "loss",
+    paper: "win",
+    scissors: "draw",
+  },
+};
+
+const play = (c) => {
+  choices.value = c;
+
+  const choices = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * choices.length);
+  computerChoice.value = choices[random];
+
+  const outcome = outcomes[c][computerChoice.value];
+
+  if (outcome === "win") {
+    wins.value++;
+  } else if (outcome === "loss") {
+    loss.value++;
+  } else {
+    draws.value++;
+  }
+};
 </script>
 
 <template>
